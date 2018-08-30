1c3f7841c5f98be99ab281f7c5027c06acb26152
ashish4497 <ak8393267@gmail.com>
2018-08-30 23:32:55 +0530

    practice

---
+++ Day 1/hello/main.js
@@ -0,0 +1,24 @@
+// first program
+// var name=prompt("your name");
+// if (name==""){
+// 	name="abc";
+// }
+// var age=45;
+// var collage=true;
+// alert(`Hello ${name} age ${age} collage ${collage}`)
+
+
+
+//for(var i=2;i<21;i=i+2){
+	// console.log(i)
+// }
+
+
+function add(a,b){
+	return console.log(a+b);
+}
+add(7,9)
+function multiply(a,b){
+	return console.log(a*b);
+}
+multiply(2,8)
