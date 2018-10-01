fef8dd517e056431269da9f3cf1aa36a9eae6809
ashish4497 <ak8393267@gmail.com>
2018-10-01 21:24:24 +0530

    search book

---
+++ day23/main.js
@@ -0,0 +1,30 @@
+var button = document.querySelector(".btn");
+button.addEventListener("click",booksearch);
+var bookList;
+var findbook = document.querySelector(".search-box");
+var bookDetail = document.querySelector(".book_list");
+
+function booksearch(){
+	var value = findbook.value;
+	var link = `https://www.googleapis.com/books/v1/volumes?q=${value}`;
+	fetch(link).then(response => response.json()).then(
+		data =>{
+			bookList = data;
+		displayBooks();
+	});
+findbook.value ="";
+}
+
+function displayBooks() {
+	var newItems = bookList.items.map(book => {
+		return (
+			`<li><img src=${book.volumeInfo.imageLinks.smallThumbnail}" class="bookimage">
+			<div class="Title">Title - ${book.volumeInfo.title}</div>
+			<div class="Authors">Author - ${book.volumeInfo.authors}</div>
+			<div class="page">Page - ${book.volumeInfo.pageCount}</div>
+			</li>
+			`
+		);
+	});
+	bookDetail.innerHTML = newItems.join(" ");
+}
