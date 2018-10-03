8adca0e79386461cf09fbf933d6211e025f80128
ashish4497 <ak8393267@gmail.com>
2018-10-03 19:25:30 +0530

    canves

---
+++ Day 25 Canvas/Project/breakout/main.js
@@ -0,0 +1,51 @@
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext('2d');
+c.fillStyle ="rgba(255 ,0, 0,0.5)";
+c.fillRect(100,100,100,100);
+c.fillStyle ="rgba(0 ,0, 255,0.5)";
+c.fillRect(400,100,100,100);
+c.fillStyle ="rgba(0 255, 0,0.5)";
+c.fillRect(300,300,100,100);
+// console.log(canvas);
+// line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = ("red");
+c.stroke();
+// arc and circle
+for ( var i =0; i<50; i++) {
+  var x = Math.random()* window.innerWidth;
+  var y = Math.random()* window.innerHeight;
+c.beginPath();
+c.arc(x, y, 30, 0, Math.PI * 2, false);
+c.strokeStyle = "blue";
+c.stroke();
+};
+var x = Math.random() *innerWidth;
+var y = Math.random() *innerHeight;
+var x =200;
+var y =300;
+dx = 2;
+dy = 2;
+var radius = 30;
+function animate() {
+ requestAnimationFrame(animate);
+	c.clearRect(0, 0,innerWidth,innerHeight);
+	c.beginPath();
+	c.arc(x, y,radius, 0, Math.PI * 2, false);
+	c.strokeStyle = "red";
+	c.stroke();
+  if(x + radius > innerWidth || x- radius < 0) {
+    dx = -dx
+  }
+  if(y + radius > innerHeight || y- radius < 0){
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+};
+animate();
