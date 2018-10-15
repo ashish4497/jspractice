16e3302001076d6c2398ab0185f8a12364d33fb4
ashish4497 <ak8393267@gmail.com>
2018-10-15 22:51:48 +0530

    drop&down

---
+++ Drop and Down/assets/drop.js
@@ -0,0 +1,26 @@
+	var lists = document.querySelectorAll("li");
+var dragValue;
+var dropValue;
+lists.forEach(list => {
+    list.addEventListener('dragstart',dragStart);
+    list.addEventListener('dragover',dragOver);
+    list.addEventListener('drop',dragEnd);
+});
+    function dragStart(e){
+    dragValue = this;
+      console.log(dragValue);
+    e.dataTransfer.dropEffect = "move";
+   
+}
+    function dragOver(e){
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+}
+    function dragEnd(e) {
+    e.preventDefault();
+    if(dragValue !== this) {
+    dropValue = this.innerHTML;
+    this.innerHTML = dragValue.innerHTML;
+    dragValue.innerHTML = dropValue;
+  }
+}
