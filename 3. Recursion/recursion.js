9f4203fe6ad95c8d0061f7026aa40abab6a22c67
ashish4497 <ak8393267@gmail.com>
2018-10-28 21:09:31 +0530

    add

---
+++ 3. Recursion/recursion.js
@@ -0,0 +1,45 @@
+//1. Write a function that loops through the numbers n down to 0.
+ function count()
+
+
+
+//2. Write a function countTo that takes one argument limit and prints number from 0 to limit
+function count(a,b){
+if(a>b) return;
+console.log(a)
+count(++a,b)
+}
+count(5,10)
+
+//3. Write a function 'exponent' that takes two arguments base, and expo,
+//uses a while loop to return the exponenet value of the base.
+function factorial(a,b){
+	if(a==)
+}
+
+
+
+//4. Write a function factorial that take 1 argument 'number' and gives the factorial of the number.
+function factorial(a){
+	if(a==1) return 1;
+	return a*factorial(--a)
+	console.log(a)
+}
+factorial(5)
+
+
+
+
+
+//5. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+
+
+
+//6. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num'
+// and multiplies each arr value into by num and returns an array of the values.
+
+
+
+// 7. Write a function 'recursiveReverse' that takes an array and uses recursion
+// to return its contents in reverse
+
--- Drop and Down/assets/drop.js
+++ Drop and Down/assets/drop.js
@@ -1,26 +1,33 @@
-	var lists = document.querySelectorAll("li");
+var lists = document.querySelectorAll("li");
+
 var dragValue;
 var dropValue;
+
+
 lists.forEach(list => {
-    list.addEventListener('dragstart',dragStart);
-    list.addEventListener('dragover',dragOver);
-    list.addEventListener('drop',dragEnd);
+  list.addEventListener('dragstart',dragStart);
+  list.addEventListener('dragover',dragOver);
+  list.addEventListener('drop',dragEnd);
 });
-    function dragStart(e){
-    dragValue = this;
-      console.log(dragValue);
-    e.dataTransfer.dropEffect = "move";
+
+
+function dragStart(e) {
+	dragValue = this;
+	console.log(dragValue);
+	e.dataTransfer.dropEffect = "move";
    
 }
-    function dragOver(e){
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
+
+function dragOver(e) {
+	e.preventDefault();
+	e.dataTransfer.dropEffect = "move";
 }
-    function dragEnd(e) {
-    e.preventDefault();
-    if(dragValue !== this) {
-    dropValue = this.innerHTML;
-    this.innerHTML = dragValue.innerHTML;
-    dragValue.innerHTML = dropValue;
+
+function dragEnd(e) {
+	e.preventDefault();
+	if(dragValue !== this) {
+	dropValue = this.innerHTML;
+	this.innerHTML = dragValue.innerHTML;
+	dragValue.innerHTML = dropValue;
   }
 }
